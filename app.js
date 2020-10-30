04fb46e168ea3c3726aafa1dc3a5aef02df20d08
Divyansh Raina <57826142+QuantumBoy-729@users.noreply.github.com>
2020-10-30 17:42:21 +0530

    Add files via upload
    Updated files, just use a normal bash file to insert python3 tsharknew.py

--- app.js
+++ app.js
@@ -1,7 +1,60 @@
-const express = require('express');
-const app = express()
+var connect = require('connect');
+var http = require('http');
+var net = require('net');
+var fs = require('fs').promises
+var app = connect();
+var requestIp = require('request-ip');
+const express = require('express')
+const appl = express()
+// you can override which attirbute the ip will be set on by
+// passing in an options object with an attributeName
+app.use(requestIp.mw({ secondTry : 'clientIp' }))
 
-app.get('/',(req,res)=>{
-    res.render('project.ejs');
-})
-app.listen(3000);
+const exec = require('child_process').exec;
+
+app.use(function(req, res) {
+    var ip = req.clientIp;
+    console.log(ip);
+    var ipType = net.isIP(ip);
+    fs.readFile('./project.html')
+    .then(contents=>{
+        res.setHeader("Content-Type","text/html");
+        res.writeHead(200);
+        res.end(contents);
+    })
+    .catch(err => {
+        res.writeHead(500);
+        res.end(err);
+        return;})
+    // }) // returns 0 for invalid, 4 for IPv4, and 6 for IPv6
+    //res.end('Hello, your ip address is ' + ip + ' and is of type IPv' + ipType + '\n');
+
+});
+
+// http.createServer((req,res)=>{
+//      fs.readFile('project.html',(err,data)=>{
+//         res.writeHead(200,{'Content-Type':'text/html'})
+//         res.write(data)
+//         return res.end()
+//     })
+// })
+// appl.get('/',(req,res)=>{
+//     res.render('project.ejs')
+
+// })
+const myShellScript = exec('sh bash.sh /project_2');
+
+const server =http.createServer(app).listen(3000);
+// server.on('request',(req,res)=>{
+//     fs.readFile(`./project.html`)
+//     .then(contents=>{
+//         res.setHeader("Content-Type","text/html");
+//         res.writeHead(200);
+//         res.end(contents);
+//     })
+    
+//     });
+// })
+// appl.listen((req,res)=>{
+//     console.log("Working")
+// },3000)
